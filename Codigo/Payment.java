226c2492a332d5e7effd7903c9ad74121380af77
Eunice Pamela Coyago Arce <75284330+EuniceCoyago@users.noreply.github.com>
2021-01-03 16:31:37 -0500

    Add files via upload

---
+++ Codigo/Payment.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.recognitionofplates.model;
+
+import java.util.Date; 
+
+/**
+ *
+ * @author EUNICE COYAGO
+ */
+public class Payment {
+    private int id;
+    private Plates idPlates;
+    private float cost;
+    private Date dateInitial;
+    private Date dateFinished;
+    
+    public void insert(){
+        
+    }
+    
+    public void deletd(){
+        
+    }
+    
+    public void update(){
+        
+    }
+    
+    public boolean calculated(){
+        
+        return true;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the idPlates
+     */
+    public Plates getIdPlates() {
+        return idPlates;
+    }
+
+    /**
+     * @param idPlates the idPlates to set
+     */
+    public void setIdPlates(Plates idPlates) {
+        this.idPlates = idPlates;
+    }
+
+    /**
+     * @return the cost
+     */
+    public float getCost() {
+        return cost;
+    }
+
+    /**
+     * @param cost the cost to set
+     */
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    /**
+     * @return the dateInitial
+     */
+    public Date getDateInitial() {
+        return dateInitial;
+    }
+
+    /**
+     * @param dateInitial the dateInitial to set
+     */
+    public void setDateInitial(Date dateInitial) {
+        this.dateInitial = dateInitial;
+    }
+
+    /**
+     * @return the dateFinished
+     */
+    public Date getDateFinished() {
+        return dateFinished;
+    }
+
+    /**
+     * @param dateFinished the dateFinished to set
+     */
+    public void setDateFinished(Date dateFinished) {
+        this.dateFinished = dateFinished;
+    }
+    
+}
