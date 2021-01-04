83d06b06aa46545b37acca2571e5542ee61e5915
Jonathan Andres Cortez Tamayo <63420391+JonathanC1@users.noreply.github.com>
2021-01-03 23:16:14 -0500

    Update Code.java

--- 06Código/Code.java
+++ 06Código/Code.java
@@ -3,11 +3,11 @@ package ec.edu.espe.recognitionofplates.model;
 import java.util.Date;   
 
 public class Payment {
-				    private int id;
-								private Plates idPlates;
-								private float cost;
-								private Date dateInitial;
-								private Date dateFinished;
+	private int id;
+	private Plates idPlates;
+	private float cost;
+	private Date dateInitial;
+	private Date dateFinished;
 
  
  }
