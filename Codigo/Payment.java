dedb0bc67a60a68827ea78e3710ab11b6aac735f
Jonathan Andres Cortez Tamayo <63420391+JonathanC1@users.noreply.github.com>
2021-01-03 19:50:56 -0500

    Update Payment.java

--- Codigo/Payment.java
+++ Codigo/Payment.java
@@ -35,72 +35,42 @@ public class Payment {
         return true;
     }
 
-    /**
-     * @return the id
-     */
     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the idPlates
-     */
     public Plates getIdPlates() {
         return idPlates;
     }
 
-    /**
-     * @param idPlates the idPlates to set
-     */
     public void setIdPlates(Plates idPlates) {
         this.idPlates = idPlates;
     }
 
-    /**
-     * @return the cost
-     */
     public float getCost() {
         return cost;
     }
 
-    /**
-     * @param cost the cost to set
-     */
     public void setCost(float cost) {
         this.cost = cost;
     }
-
-    /**
-     * @return the dateInitial
-     */
+    
     public Date getDateInitial() {
         return dateInitial;
     }
 
-    /**
-     * @param dateInitial the dateInitial to set
-     */
     public void setDateInitial(Date dateInitial) {
         this.dateInitial = dateInitial;
     }
 
-    /**
-     * @return the dateFinished
-     */
     public Date getDateFinished() {
         return dateFinished;
     }
 
-    /**
-     * @param dateFinished the dateFinished to set
-     */
     public void setDateFinished(Date dateFinished) {
         this.dateFinished = dateFinished;
     }
