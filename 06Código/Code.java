a94b75ebbcddaf3b1ecafe5f0f6157384f7c1685
Jonathan Andres Cortez Tamayo <63420391+JonathanC1@users.noreply.github.com>
2021-01-04 07:48:59 -0500

    Update Code.java

--- 06Código/Code.java
+++ 06Código/Code.java
@@ -8,8 +8,73 @@ public class Payment {
 	private float cost;
 	private Date dateInitial;
 	private Date dateFinished;
+	
+	    public void insert(){
+        
+	    }
 
- 
+	    public void deletd(){
+
+	    }
+
+	    public void update(){
+
+	    }
+
+	    public boolean calculated(){
+
+		return true;
+	    }
+
+
+	    public int getId() {
+		return id;
+	    }
+
+
+	    public void setId(int id) {
+		this.id = id;
+	    }
+
+
+	    public Plates getIdPlates() {
+		return idPlates;
+	    }
+
+
+	    public void setIdPlates(Plates idPlates) {
+		this.idPlates = idPlates;
+	    }
+
+
+	    public float getCost() {
+		return cost;
+	    }
+
+
+	    public void setCost(float cost) {
+		this.cost = cost;
+	    }
+
+	    public Date getDateInitial() {
+		return dateInitial;
+	    }
+
+
+	    public void setDateInitial(Date dateInitial) {
+		this.dateInitial = dateInitial;
+	    }
+
+
+	    public Date getDateFinished() {
+		return dateFinished;
+	    }
+
+
+	    public void setDateFinished(Date dateFinished) {
+		this.dateFinished = dateFinished;
+	    }
+
  }
 public class IDImage {
     
