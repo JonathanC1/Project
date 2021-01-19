510967ca2c2ebe80b9d321c78b0fc7af80b53fef
Milton Roberto Cuji Revelo <mrcuji@espe.edu.ec>
2021-01-19 14:18:31 -0500

    update

--- 06Código/recognitionOfPlates/recognitionOfPlates/src/recognitionofplates/RecognitionOfPlates.java
+++ 06Código/recognitionOfPlates/recognitionOfPlates/src/recognitionofplates/RecognitionOfPlates.java
@@ -4,6 +4,13 @@
  * and open the template in the editor.
  */
 package recognitionofplates;
+import ec.edu.espe.FileMananger.utils.Data;
+import ec.edu.espe.recognition_ofplates.model.Person;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Scanner;
 
 /**
  *
@@ -30,6 +37,127 @@ public class RecognitionOfPlates {
         System.out.println("-------------------------------------------------");
         System.out.println("----------------SIGMA PROGRAMMERS----------------\n");
     
+    }
+    public static void inData() throws IOException{
+       Scanner read = new Scanner(System.in);
+       
+       System.out.print("Input your name: ");
+       String name = read.nextLine();
+       System.out.print(name + " Input your lastname: ");
+       String lastName = read.nextLine();
+       System.out.print(name + " Input your plate: ");
+       String plate = read.nextLine();
+       Person person = new Person(name,lastName,plate);
+       String saveData = person.toString();
+       Data.save("Information.csv",saveData + "\r");
+       outData(name,lastName,plate);
+       
+   }
+    
+    public static void searchData(String plate){
+    
+    
+        
+        
+        
+        
+    
+    }
+   
+    public static void outData(String name, String lastName, String plate){
+       System.out.println(" ");
+       System.out.println("NAME: " + name);
+       System.out.println("LASTNAME: " + lastName);
+       System.out.println("PLATE: " + plate);   
+   }
+    
+    public static void student(){
+        Scanner read = new Scanner(System.in);
+        int option = 0;
+        
+        do{
+            option = read.nextInt();
+            switch(option){
+                case 1:{
+                    try {
+                        inData();
+                    } catch (IOException ex) {
+                        Logger.getLogger(RecognitionOfPlates.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    break;
+                }
+                
+                case 2:{
+                    System.out.println("Thank for use the program :)");
+                    break;
+                }
+                default:{
+                    System.out.println("option no recognized");
+                    break;
+                }
+            }
+        }while(option!=2);
+    
+    }  
+    
+    public static void teacher(){
+        Scanner read = new Scanner(System.in);
+        int option = 0;
+        
+        do{
+            option = read.nextInt();
+            switch(option){
+                case 1:{
+                    try {
+                        inData();
+                    } catch (IOException ex) {
+                        Logger.getLogger(RecognitionOfPlates.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    break;
+                }
+
+                case 2:{
+                    System.out.println("What word going to search? ");
+                    String word = read.next();
+                    Data.find(new File("find.csv"),word);
+                    break;
+                }
+
+                case 3:{
+                    System.out.println("\nThank for use the program :)");
+                    break;
+                }
+                default:{
+                    System.out.println("\nOption no recognized.");
+                    break;
+                }
+            }
+        }while(option!=3);
+    
+    } 
+    
+    public static void main(String[] args){
+        
+        
+        Scanner read = new Scanner(System.in);
+        
+        welcome();
+        System.out.println("\nEnter the start data");
+        System.out.print("USER: ");
+        String user = read.next();
+        System.out.print("PASSWORD: ");
+        String password = read.next();
+        
+        
+        if(user.equalsIgnoreCase("Student") && password.equalsIgnoreCase("012345")){
+            menu1();
+            student();
+        }else if(user.equalsIgnoreCase("Teacher") && password.equalsIgnoreCase("123450")){
+            menu();
+            teacher();
+        }else{
+            System.out.println("\nUSER OR PASSWORD INCORRECT!!");
+        }
     }
     
 }
