18769d11a658a654dbba1e4973f36bca5d31af74
Milton Roberto Cuji Revelo <mrcuji@espe.edu.ec>
2021-01-19 13:27:39 -0500

    RecognitionOfPlates

---
+++ 06Código/recognitionOfPlates/recognitionOfPlates/src/ec/edu/espe/recognitionofplates/model/Plates.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.recognitionofplates.model;
+
+/**
+ *
+ * @author Group2
+ */
+public class Plates {
+    private int id;
+    private String description;
+    
+    public void insert(){
+        
+    }
+    
+    public void delete(){
+        
+    }
+    
+    public void update(){
+        
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
