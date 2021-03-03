847804475cdc3e8bb14c8da979903e09a5e5bd93
Dino4U <dinochris@hotmail.es>
2021-03-03 00:53:36 -0500

    Class products update

--- Mechanic/06Code/Mechanic/src/ec/edu/espe/mechanic/model/Products.java
+++ Mechanic/06Code/Mechanic/src/ec/edu/espe/mechanic/model/Products.java
@@ -10,8 +10,64 @@ package ec.edu.espe.mechanic.model;
  * @author ELEN
  */
 public class Products {
-    String name ;
-    int quanty;
+    private String name ;
+    private int quanty;
+    private int price;
+
+    public Products(String name, int quanty, int price) {
+        this.name = name;
+        this.quanty = quanty;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Products{" + "name=" + name + ", quanty=" + quanty + ", price=" + price + '}';
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the quanty
+     */
+    public int getQuanty() {
+        return quanty;
+    }
+
+    /**
+     * @param quanty the quanty to set
+     */
+    public void setQuanty(int quanty) {
+        this.quanty = quanty;
+    }
+
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+    
         
     
 }
