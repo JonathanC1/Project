6449e2d365796537255f3d5e0e001cd2f09ba6f8
Milton Roberto Cuji Revelo <mrcuji@espe.edu.ec>
2021-04-05 02:28:22 -0500

    Refactor the classes and author

--- Mechanic/06Code/Mechanic/src/ec/edu/espe/mechanic/view/FrmWorkerRecord.java
+++ Mechanic/06Code/Mechanic/src/ec/edu/espe/mechanic/view/FrmWorkerRecord.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.espe.mechanic.view;
 
 import javax.swing.JOptionPane;
@@ -10,7 +5,7 @@ import javax.swing.table.DefaultTableModel;
 
 /**
  *
- * @author Roberth-C
+ * @author Sigma Programmers
  */
 public class FrmWorkerRecord extends javax.swing.JFrame {
     DefaultTableModel modelo;
